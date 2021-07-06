62aa4409ba63e248d8f86cf14ac47e8ae5fdc533
Ethan Ho <eho@tacc.utexas.edu>
2021-07-06 12:34:04 -0500

    Actions skips tests that require Tapis auth cache

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -3,31 +3,34 @@ import pytest
 from agavepy.agave import Agave
 
 
-def test_client_v2(client_v2):
-	"""Test fixture `client_v2`"""
-	assert isinstance(client_v2, Agave)
-	_ = client_v2.apps.list()
+@pytest.mark.tapis_auth
+class TestFixtures:
+    
+	def test_client_v2(self, client_v2):
+		"""Test fixture `client_v2`"""
+		assert isinstance(client_v2, Agave)
+		_ = client_v2.apps.list()
 
 
-def test_token_v2(token_v2):
-	"""Test fixture `token_v2`"""
-	assert isinstance(token_v2, str)
-	assert token_v2
+	def test_token_v2(self, token_v2):
+		"""Test fixture `token_v2`"""
+		assert isinstance(token_v2, str)
+		assert token_v2
 
 
-def test_abaco_username(abaco_username):
-    """Test fixture `abaco_username`"""
-    assert abaco_username is None
-    assert '_abaco_username' in os.environ
+	def test_abaco_username(self, abaco_username):
+		"""Test fixture `abaco_username`"""
+		assert abaco_username is None
+		assert '_abaco_username' in os.environ
 
 
-def test_abaco_api_server(abaco_api_server):
-    """Test fixture `abaco_api_server`"""
-    assert abaco_api_server is None
-    assert '_abaco_api_server' in os.environ
+	def test_abaco_api_server(self, abaco_api_server):
+		"""Test fixture `abaco_api_server`"""
+		assert abaco_api_server is None
+		assert '_abaco_api_server' in os.environ
 
 
-def test_abaco_access_token(abaco_access_token):
-    """Test fixture `abaco_access_token`"""
-    assert abaco_access_token is None
-    assert '_abaco_access_token' in os.environ
+	def test_abaco_access_token(self, abaco_access_token):
+		"""Test fixture `abaco_access_token`"""
+		assert abaco_access_token is None
+		assert '_abaco_access_token' in os.environ
