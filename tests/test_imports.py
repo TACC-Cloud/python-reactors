4df2047444fd99bec5cf7c4224c556af616db3eb
Ethan Ho <eho@tacc.utexas.edu>
2021-07-07 13:04:14 -0500

    Re-enable import tests

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,5 @@
 import pytest
-import os
-import sys
+
 
 def test_module_imports():
     from reactors.runtime import Reactor
@@ -13,4 +12,3 @@ def test_module_imports():
     # from reactors import context
     # from reactors import jsonmessages
     from reactors import version
-    return True
