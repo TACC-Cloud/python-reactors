2c45e671e24460f9d63f4460c95e16da3977a9cf
Ethan Ho <eho@tacc.utexas.edu>
2021-08-11 14:45:01 -0500

    Exhaustive unit testing of find_schema_files
    All existing schema tests passing without skips

--- tests/test_jsonmessage.py
+++ tests/test_jsonmessage.py
@@ -10,6 +10,16 @@ def r(r_tp_opt):
     return r_tp_opt
 
 
+@pytest.fixture
+def R(R_tp_opt):
+    return R_tp_opt
+
+
+@pytest.fixture
+def schema_test_dir():
+    return os.path.join(os.getcwd(), 'tests', 'data', 'abacoschemas')
+
+
 def test_validate_named_message_jsonschema(r):
     '''Ensure singular message.jsonschema will validate'''
     message = json.loads('{"key": "value"}')
@@ -47,31 +57,54 @@ def test_load_schema(reference, success):
             sch = jsonmessages.load_schema(reference)
 
 
-def test_find_schema_files_static():
+def test_find_schema_files(change_test_dir, schema_test_dir):
     '''Test that >1 schema files can be discovered in the test environment'''
-    static_paths = [os.path.join('tests', 'data', 'abacoschemas')]
-    for d in static_paths:
-        assert os.path.isdir(d)
-    schema_files = jsonmessages.find_schema_files(static_paths=static_paths)
-    assert len(schema_files) >= 1
+    assert not jsonmessages.find_schema_files()
+    change_test_dir(schema_test_dir)
+    schema_files = jsonmessages.find_schema_files()
+    assert len(schema_files) >= 2
 
 
-@pytest.mark.skip
-def test_classify_simple_json_message():
+def test_find_schema_files_static_paths(schema_test_dir):
+    '''Test that >1 schema files can be discovered in the test environment
+    when using `static_paths` kwarg.'''
+    schema_files = jsonmessages.find_schema_files(static_paths=[schema_test_dir])
+    og_n_matches = len(schema_files)
+    assert og_n_matches >= 2
+
+
+def test_find_schema_files_no_dup(change_test_dir, static_paths):
+    '''Test that >1 schema files added from `static_paths` kwarg are not 
+    appended as duplicates if they were already found.
+    '''
+    change_test_dir(schema_test_dir)
+    no_static = jsonmessages.find_schema_files()
+    og_n_matches = len(schema_files)
+    assert og_n_matches >= 2
+
+    # check that matches are not added twice if using static_paths
+    w_matches = jsonmessages.find_schema_files(static_paths=static_paths)
+    assert len(w_matches) == og_n_matches
+
+
+def test_classify_simple_json_message(R, change_test_dir):
     '''Test that simple JSON can be classified with the generic schema'''
-    r = Reactor()
+    r = R()
     message = json.loads('{"aljsydgflajsgd": "FKJHFKJLJHGL345678"}')
     matches = message_module.classify_message(message, permissive=True)
     assert len(matches) == 1
-    assert 'abaco_json_message' in matches
+    assert 'abaco_json_message' in [m['$id'] for m in matches]
 
 
-@pytest.mark.skip
-def test_classify_email_json_message():
+def test_classify_email_json_message(R, change_test_dir):
     '''Test that an email message can be classified with the generic and email message schema'''
-    r = Reactor()
+    r = R()
     message = json.loads('{"to": "tacc@email.tacc.cloud"}')
     matches = message_module.classify_message(message)
-    assert len(matches) >= 1
-    assert 'abaco_json_email' in matches
+    match_ids = list()
+    for match in matches:
+        assert isinstance(match, dict)
+        match_ids.append(match['$id'])
+    assert 'abaco_json_email' in [m['$id'] for m in matches]
+    assert 'abaco_json_message' in [m['$id'] for m in matches]
 
