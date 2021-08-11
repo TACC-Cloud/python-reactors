b45cd805140a24e17d9f79a2bbe2429f4f309d8c
Ethan Ho <eho@tacc.utexas.edu>
2021-08-11 13:01:21 -0500

    Refactor test_reactors_send_message

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,20 +1,18 @@
 import pytest
-import os
-import sys
-
 from agavepy.agave import AgaveError
-from reactors.runtime import Reactor
 
-@pytest.mark.skip
-@pytest.mark.parametrize("actor_id, message, success", [
-    ('4xvmNVBxeEDRN', {'text': '4xvmNVBxeEDRN'}, True),
-    # ('sd2eadm-listener', {'text': 'sd2eadm-listener'}, True),
-    ('meep-meep-meep', {'text': 'meep-meep-meep'}, False)])
-def test_reactors_send_message(actor_id, message, success):
-    r = Reactor()
-    if success is True:
-        exec_id = r.send_message(actor_id, message, retryMaxAttempts=1, ignoreErrors=False)
-        assert exec_id is not None
-    else:
-        with pytest.raises(AgaveError):
-            r.send_message(actor_id, message, retryMaxAttempts=1, ignoreErrors=False)
+
+@pytest.fixture
+def r(r_bare):
+    return r_bare
+
+
+def test_reactors_send_message(r, actor_wc):
+    message = 'hello, world'
+    # passes when sending to a real actor
+    exec_id = r.send_message(actor_wc['id'], message, retryMaxAttempts=1, ignoreErrors=False)
+    assert exec_id is not None
+
+    # fails when sending to actor that DNE
+    with pytest.raises(AgaveError):
+        r.send_message('foobarbaz', message, retryMaxAttempts=1, ignoreErrors=False)
