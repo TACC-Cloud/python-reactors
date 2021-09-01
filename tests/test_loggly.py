e707d80d9c194488727882ec5da183ee289879a2
Ethan Ho <eho@tacc.utexas.edu>
2021-09-01 17:21:12 -0500

    New submodule for LogglyHandler tests

--- tests/test_loggly.py
+++ tests/test_loggly.py
@@ -1,12 +1,21 @@
 def test_token_from_env(R_tp_opt, monkeypatch):
-	"""Check that Loggly token in environment is valid."""
-	monkeypatch.setenv('_REACTOR_LOGGLY_CUSTOMER_TOKEN', 'VewyVewySekwit')
-	r = R_tp_opt()
-	assert r.settings.loggly.customer_token == 'VewyVewySekwit'
+    """Check that Loggly token in environment is valid."""
+    monkeypatch.setenv('_REACTOR_LOGGLY_CUSTOMER_TOKEN', 'VewyVewySekwit')
+    r = R_tp_opt()
+    assert r.settings.loggly.customer_token == 'VewyVewySekwit'
 
 
 def test_url_from_env(R_tp_opt, monkeypatch):
-	"""Check that Loggly token in environment is valid."""
-	monkeypatch.setenv('_REACTOR_LOGGLY_URL', 'VewyVewySekwit')
-	r = R_tp_opt()
-	assert r.settings.loggly.url == 'VewyVewySekwit'
+    """Check that Loggly token in environment is valid."""
+    monkeypatch.setenv('_REACTOR_LOGGLY_URL', 'VewyVewySekwit')
+    r = R_tp_opt()
+    assert r.settings.loggly.url == 'VewyVewySekwit'
+
+
+def test_loggly_handler(R_tp_opt, loggly_url, monkeypatch):
+    """Reading Loggly URL from env var, can POST to Loggly using the
+    LogglyHandler.
+    """
+    monkeypatch.setenv('_REACTOR_LOGGLY_URL', loggly_url)
+    r = R_tp_opt()
+    r.logger.info('Hello from LogglyHandler')
