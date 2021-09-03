6972e9e78608d4f4a032b07e99e094e4ccf2737d
Ethan Ho <eho@tacc.utexas.edu>
2021-09-03 16:36:15 -0500

    Clean up import

--- src/reactors/logtypes/loggly.py
+++ src/reactors/logtypes/loggly.py
@@ -1,12 +1,1 @@
-import json
-import logging
-import logging.handlers
-import sys
-import traceback
-
-import requests
-
-try:
-    from .loggly_futures_session import LogglyHandler
-except Exception as exc:
-    raise Exception(exc)
+from .loggly_futures_session import LogglyHandler
