25fa3e4e23ff2e45a4965c1393cf3630f1051e8f
Ethan Ho <eho@tacc.utexas.edu>
2021-09-03 15:03:20 -0500

    Need to change env name to avoid Loggly posting every logging call

--- tests/fixtures/log.py
+++ tests/fixtures/log.py
@@ -4,10 +4,10 @@ import os
 
 @pytest.fixture
 def loggly_url(monkeypatch):
-    if "_REACTOR_LOGGLY_URL" in os.environ:
-        return os.environ['_REACTOR_LOGGLY_URL']
-    elif '_REACTOR_LOGGLY_CUSTOMER_TOKEN' in os.environ:
-        token = os.environ['_REACTOR_LOGGLY_CUSTOMER_TOKEN']
+    if "_TEST_LOGGLY_URL" in os.environ:
+        return os.environ['_TEST_LOGGLY_URL']
+    elif '_TEST_LOGGLY_CUSTOMER_TOKEN' in os.environ:
+        token = os.environ['_TEST_LOGGLY_CUSTOMER_TOKEN']
         return f'https://logs-01.loggly.com/inputs/{token}/tag/python'
-    raise ValueError(f"neither _REACTOR_LOGGLY_URL nor "
-                        "_REACTOR_LOGGLY_CUSTOMER_TOKEN are set in test env")
+    raise ValueError(f"neither _TEST_LOGGLY_URL nor "
+                        "_TEST_LOGGLY_CUSTOMER_TOKEN are set in test env")
