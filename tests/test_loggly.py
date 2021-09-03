dfa2bfb13f9bc36b5c75a53e50a568f8fa5ab968
Ethan Ho <eho@tacc.utexas.edu>
2021-09-03 15:49:51 -0500

    Check that logging fails with bogus customer_token

--- tests/test_loggly.py
+++ tests/test_loggly.py
@@ -4,19 +4,28 @@ import logging
 
 
 def test_loggly_handler(R_tp_opt, loggly_token, monkeypatch):
-    """Reading Loggly URL from env var, can POST to Loggly using the
+    """Reading Loggly customer token from env var, can POST to Loggly using the
     LogglyHandler.
     """
     monkeypatch.setenv('_REACTOR_LOGGLY_CUSTOMER_TOKEN', loggly_token)
     r = R_tp_opt()
+
+    # get the LogglyHandler instance
     assert isinstance(r.loggers.loggly, logging.Logger), type(r.loggers.loggly)
     lhs = [handler for handler in r.loggers.loggly.handlers 
            if isinstance(handler, LogglyHandler)]
     assert len(lhs) == 1
     lh = lhs[0]
     assert isinstance(lh, LogglyHandler)
+
+    # try posting and check the status code
     assert not hasattr(lh, '_resp')
     r.loggers.loggly.info('hello from LogglyHandler')
     assert hasattr(lh, '_resp')
-    # check the response code from the last emit call
     assert lh._resp.result().status_code == 200
+
+    # now bork the token and check that the response fails to POST
+    lh.config['customer_token'] = 'bogus_token'
+    r.loggers.loggly.info('this message from LogglyHandler wont go through')
+    assert lh._resp.result().status_code != 200
+
