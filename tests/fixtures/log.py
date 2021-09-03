8ba1be4e02fb81051a4e1184aebce0415afb4e82
Ethan Ho <eho@tacc.utexas.edu>
2021-09-03 14:55:06 -0500

    Fixture expects correct ENV name

--- tests/fixtures/log.py
+++ tests/fixtures/log.py
@@ -6,8 +6,8 @@ import os
 def loggly_url(monkeypatch):
     if "_REACTOR_LOGGLY_URL" in os.environ:
         return os.environ['_REACTOR_LOGGLY_URL']
-    elif '_REACTOR_LOGGLY_TOKEN' in os.environ:
-        token = os.environ['_REACTOR_LOGGLY_TOKEN']
+    elif '_REACTOR_LOGGLY_CUSTOMER_TOKEN' in os.environ:
+        token = os.environ['_REACTOR_LOGGLY_CUSTOMER_TOKEN']
         return f'https://logs-01.loggly.com/inputs/{token}/tag/python'
     raise ValueError(f"neither _REACTOR_LOGGLY_URL nor "
-                        "_REACTOR_LOGGLY_TOKEN are set in test env")
+                        "_REACTOR_LOGGLY_CUSTOMER_TOKEN are set in test env")
